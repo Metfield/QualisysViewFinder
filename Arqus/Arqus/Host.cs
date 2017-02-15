d31b496b0294bd3751797f33f673ba6c5a6a2801
Mathmoose <materbyl@gmail.com>
2017-02-15 16:40:17 +0100

    Fixed merge issue

---
+++ Arqus/Arqus/Host.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arqus.Connection
+{
+    class Host
+    {
+        public string Name { private set; get; }
+        public string Address { private set; get; }
+
+        public Host(string name, string address)
+        {
+            this.Name = name;
+            this.Address = address;
+        }
+    }
+}
---
+++ Arqus/Arqus/HostList.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using Xamarin.Forms;
+using Arqus.Connection;
+
+namespace Arqus
+{
+	public partial class HostList : ListView
+	{
+		public HostList ()
+		{
+			InitializeComponent ();
+
+            ItemsSource = new List<Host>
+            {
+                new Host("mac.local", "192.123.123.12"),
+                new Host("not.mac.loca", "197.123.12.3")
+            };
+		}
+	}
+}

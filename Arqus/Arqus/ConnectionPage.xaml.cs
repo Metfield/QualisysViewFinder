658c1d04e71a118cacd06f41f76f3aedda208722
Mathmoose <materbyl@gmail.com>
2017-02-20 10:30:46 +0100

    Fixed Merge

--- Arqus/Arqus/ConnectionPage.xaml.cs
+++ Arqus/Arqus/ConnectionPage.xaml.cs
@@ -1,61 +1,4 @@
-﻿using System;
-using System.Net;
-using Xamarin.Forms;
-
-using QTMRealTimeSDK;
-using QTMRealTimeSDK.Data;
-using System.Collections.Generic;
-using System.Diagnostics;
-
-namespace Arqus
-{
-	public partial class ConnectionPage : ContentPage
-	{
-		public ConnectionPage()
-		{
-			InitializeComponent();
-		}
-
-        // "Connect" button was pressed
-        void OnConnectionButtonClicked(object sender, EventArgs args)
-        {
-            
-           
-        }
-
-        /// <summary>
-        /// Makes sure ipAddress string is a valid IPv4
-        /// </summary>
-        /// <param name="ipString">Holds QTM instance IP address</param>
-        /// <returns></returns>
-        public bool IsValidIPv4(string ipString)
-        {
-            // Check for null string
-            if(ipString == null)            
-                return false;            
-
-            // Check if it's made of four elements
-            if (ipString.Split(new char[] { '.' }, StringSplitOptions.RemoveEmptyEntries).Length != 4)
-                return false;
-
-            IPAddress address;
-
-            // Check if this is a valid IP address
-            if (IPAddress.TryParse(ipString, out address))
-            {
-                // Make sure it's an ipv4 (although it should)
-                if (address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
-                {
-                    return true;
-                }
-            }
-
-            // TODO: Check if address is in LAN and in a valid range!
-
-            return false;
-        }
-    }
-}
+﻿using Xamarin.Forms;
 
 namespace Arqus
 {
@@ -68,45 +11,6 @@ namespace Arqus
 			InitializeComponent();
             BindingContext = viewModel;
 		}
-
-        // "Connect" button was pressed
-        void OnConnectionButtonClicked(object sender, EventArgs args)
-        {
-            
-           
-        }
-
-        /// <summary>
-        /// Makes sure ipAddress string is a valid IPv4
-        /// </summary>
-        /// <param name="ipString">Holds QTM instance IP address</param>
-        /// <returns></returns>
-        public bool IsValidIPv4(string ipString)
-        {
-            // Check for null string
-            if(ipString == null)            
-                return false;            
-
-            // Check if it's made of four elements
-            if (ipString.Split(new char[] { '.' }, StringSplitOptions.RemoveEmptyEntries).Length != 4)
-                return false;
-
-            IPAddress address;
-
-            // Check if this is a valid IP address
-            if (IPAddress.TryParse(ipString, out address))
-            {
-                // Make sure it's an ipv4 (although it should)
-                if (address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
-                {
-                    return true;
-                }
-            }
-
-            // TODO: Check if address is in LAN and in a valid range!
-
-            return false;
-        }
 
         protected override void OnAppearing()
         {

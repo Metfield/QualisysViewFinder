fd589a4c76ba3998ba18601adb1f8fc87197e6ff
Mathmoose <materbyl@gmail.com>
2017-02-20 17:00:19 +0100

    Created Tracking2DPage
    + Tracking2DPage/ViewModel

---
+++ Arqus/Arqus/2DTrackingPage/Tracking2DPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Arqus
+{ 
+	public partial class Tracking2DPage : ContentPage
+	{
+		public Tracking2DPage ()
+		{
+			InitializeComponent();
+		}
+	}
+}
--- Arqus/Arqus/ConnectionPage/ConnectionPage.xaml.cs
+++ Arqus/Arqus/ConnectionPage/ConnectionPage.xaml.cs
@@ -4,19 +4,9 @@ namespace Arqus
 {
 	public partial class ConnectionPage : ContentPage
 	{
-        private ConnectionViewModel viewModel = new ConnectionViewModel();
-
 		public ConnectionPage()
 		{
 			InitializeComponent();
-            BindingContext = viewModel;
 		}
-
-        protected override void OnAppearing()
-        {
-            base.OnAppearing();
-
-            viewModel.LoadQTMServers();
-        }
     }
 }

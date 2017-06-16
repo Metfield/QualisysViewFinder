a8edef838cbe2071e76450f5de5496137eb8f15c
Mathmoose <materbyl@gmail.com>
2017-06-16 14:57:24 +0200

    Ref: Camera.cs

--- Arqus/Arqus/Pages/CameraPage/Camera.cs
+++ Arqus/Arqus/Pages/CameraPage/Camera.cs
@@ -1,48 +1,36 @@
-﻿using Arqus.Helpers;
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
 
+using Arqus.Helpers;
 using Arqus.Service;
-
 using Arqus.Visualization;
+
+using QTMRealTimeSDK;
+using QTMRealTimeSDK.Settings;
 
 using Prism.Mvvm;
 
-using QTMRealTimeSDK;
-
-using QTMRealTimeSDK.Settings;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Threading.Tasks;
-
-
-
 namespace Arqus.DataModels
-
 {
-
-    
-
+    /// <summary>
+    /// This camera class is supposed to be a model that reflects
+    /// the state of the cameras inside the QTM host. Anything that
+    /// changes in this model should also be updated on the QTM host.
+    /// The purpose of this class is to act as a middleware between
+    /// QTM and the Arqus application to properly update values.
+    /// </summary>
     public class Camera : BindableBase
-
     {
-
-        // public properties
-
         public ImageResolution ImageResolution { get; private set; }
 
-        
-
         public CameraScreen Parent { get; set; }
-
         
-
         public int ID { get; private set; }
 
-        private CameraMode mode;
-
-        public CameraMode Mode { get { return mode;  }  set { SetProperty(ref mode, value); } }
-
         public int Orientation { get; set; }
+
+        private CameraMode mode;
+        public CameraMode Mode { get { return mode; } set { SetProperty(ref mode, value); } }
 
         private SettingsGeneralCameraSystem settings;
         public SettingsGeneralCameraSystem Settings {
@@ -52,67 +40,35 @@ namespace Arqus.DataModels
                 SetProperty(ref settings, value);
             }
         }
-
         
-
-
-
         public Camera(int id, SettingsGeneralCameraSystem settings, ImageResolution imageResolution)
-
         {
-
             ID = id;
-
             // TODO: this is part of the settings
-
             Mode = settings.Mode;
-
             Settings = settings;
-
             ImageResolution = imageResolution;
 
-
-
             // TODO: this should not have to be done in the constructor
-
             if (IsImageMode())
-
                 EnableImageMode();
-
         }
 
-
-
         /// <summary>
-
         /// Set the camera stream mode
-
         /// </summary>
-
         /// <param name="mode"></param>
-
         public void SetMode(CameraMode mode)
-
         {
-
             // Update mode if not already running in that mode
-
             if(Mode != mode)
-
             {
-
                 if (SettingsService.SetCameraMode(ID, mode))
-
                 {
-
                     Mode = mode;
-
                     MessagingService.Send(this, MessageSubject.STREAM_MODE_CHANGED.ToString() + ID, Mode);
-
                 }
-
             }
-
         }
         
         /// <summary>
@@ -123,8 +79,6 @@ namespace Arqus.DataModels
             Debug.WriteLine("Updating settings for camera");
             UpdateSettings(SettingsService.GetCameraSettings(ID));
         }
-
-
 
         /// <summary>
         /// Update the settings of the camera if the QTM host updated the settings successfully
@@ -138,116 +92,65 @@ namespace Arqus.DataModels
             }
         }
 
-        
-
         /// <summary>
-
         /// Set a certain setting for this camera
-
         /// </summary>
-
         /// <param name="setting">the settings to be set</param>
-
         /// <param name="value">the value to set the setting to</param>
-
         public void SetSetting(string setting, double value)
         {
             if (SettingsService.SetCameraSettings(ID, setting, value.ToString()))
                 UpdateSettings();
         }
-
-
-
+        
         /// <summary>
-
-        /// Select the camera
-
+        /// Select the camera and enable the led-ring
         /// </summary>
-
         public void Select()
-
         {
-
             // Enable image mode if neccessary
-
             if(IsImageMode())
-
                 EnableImageMode();
-
-
+            
 
             Task.Run(() => SettingsService.SetLED(ID, SettingsService.LEDMode.On, SettingsService.LEDColor.Amber));
-
         }
-
-
-
-
-
+        
+        /// <summary>
+        /// Deselects the camera and disables the led-ring
+        /// </summary>
         public void Deselect()
-
         {
-
             // Disable image mode when not selected to keep the transferred data to a minimum
-
             DisableImageMode();
 
-
-
             Task.Run(() => SettingsService.SetLED(ID, SettingsService.LEDMode.Off));
-
         }
-
         
-
         /// <summary>
-
         /// Enable image mode for streaming on the QTM host
-
         /// </summary>
-
         public void EnableImageMode()
-
         {
-
             SettingsService.EnableImageMode(ID, true, ImageResolution.Width, ImageResolution.Height);
-
         }
-
-
-
+        
         /// <summary>
-
         /// Disable image mode for streaming on the QTM host
-
         /// </summary>
-
         public void DisableImageMode()
-
         {
-
             SettingsService.EnableImageMode(ID, false, ImageResolution.Width, ImageResolution.Height);
-
         }
-
-
-
+        
         /// <summary>
-
         /// Check to see if the camera is in image mode, such as video or intensity
-
         /// </summary>
-
         /// <returns>true if the camera is running in image mode</returns>
-
         private bool IsImageMode()
-
         {
-
             return Mode != CameraMode.ModeMarker;
-
         }
-
     }
 
 }

9a2498989c330d841afcb3c00d11dd7a589419ce
ebs <batis@student.chalmers.se>
2017-07-07 15:46:03 +0200

    * Updated icons

--- Arqus/Arqus/Helpers/Constants.cs
+++ Arqus/Arqus/Helpers/Constants.cs
@@ -26,10 +26,10 @@ namespace Arqus.Helpers
         public const string LENS_APERTURE_PACKET_STRING = "Aperture";
 
         // Mode toolbar icon constants
-        public const string MODEBAR_ICON_VIDEO_NORMAL = "drawable-hdpi/ic_camera_white_24dp.png";
-        public const string MODEBAR_ICON_VIDEO_DEMO = "drawable-hdpi/ic_camera_grey_800_24dp.png";
-        public const string MODEBAR_ICON_INTENSITY_NORMAL = "drawable-hdpi/ic_exposure_white_24dp.png";
-        public const string MODEBAR_ICON_INTENSITY_DEMO = "drawable-hdpi/ic_exposure_grey_800_24dp.png";
+        public const string MODEBAR_ICON_VIDEO_NORMAL = "ic_videocam_white_24dp";
+        public const string MODEBAR_ICON_VIDEO_DEMO = "ic_videocam_grey_800_24dp";
+        public const string MODEBAR_ICON_INTENSITY_NORMAL = "ic_intensity_white_24dp";
+        public const string MODEBAR_ICON_INTENSITY_DEMO = "ic_intensity_grey_800_24dp";
 
         /// <summary>
         /// Navigation strings
@@ -37,6 +37,6 @@ namespace Arqus.Helpers
         public const string NAVIGATION_DEMO_MODE_STRING = "demoMode";
 
         // Page titles
-        public const string TITLE_GRIDVIEW = "Camera Overview";
+        public const string TITLE_GRIDVIEW = "Select a camera";
     }
 }

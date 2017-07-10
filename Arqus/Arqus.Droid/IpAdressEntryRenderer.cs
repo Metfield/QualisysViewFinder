5a3dd1424dc9eb489527013026bc1f93719fc773
Mathmoose <materbyl@gmail.com>
2017-07-10 17:38:15 +0200

    First

---
+++ Arqus/Arqus.Droid/IpAdressEntryRenderer.cs
@@ -0,0 +1,22 @@
+﻿using Android.Text.Method;
+using Arqus.Droid.Renderers;
+using Arqus.UI;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(IpAdressEntryRenderer), typeof(CustomIpAdressEntryRenderer))]
+namespace Arqus.Droid.Renderers
+{
+    public class CustomIpAdressEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            var native = Control as EntryEditText;
+
+            native.InputType = Android.Text.InputTypes.ClassNumber | Android.Text.InputTypes.NumberFlagDecimal | Android.Text.InputTypes.NumberFlagSigned;
+            native.KeyListener = DigitsKeyListener.GetInstance("1234567890.");
+        }
+    }
+}

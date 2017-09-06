3d0e48553cf2ac0a12e8eae4ccffe9d7291ba1de
Martin Gejke <martin.gejke@qualisys.se>
2017-09-06 09:44:20 +0200

    * Updated version info etc

--- Arqus/Arqus.iOS/Properties/AssemblyInfo.cs
+++ Arqus/Arqus.iOS/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Arqus")]
+[assembly: AssemblyTitle("Qualisys Viewfinder")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Arqus")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Qualisys AB")]
+[assembly: AssemblyProduct("Qualisys Viewfinder")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 

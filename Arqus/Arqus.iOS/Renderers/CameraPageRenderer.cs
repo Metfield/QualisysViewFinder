e36bc7ba57e1eb2e0769869eac7cf0624cd822b9
ebs <batis@student.chalmers.se>
2017-10-11 14:54:19 +0200

    [iOS] Disables swipe-to-go-back gesture

---
+++ Arqus/Arqus.iOS/Renderers/CameraPageRenderer.cs
@@ -0,0 +1,21 @@
+﻿using Arqus;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly:ExportRenderer(typeof(CameraPage), typeof(CameraPageRenderer))]
+namespace Arqus
+{
+    // iOS-specific renderer for CameraPage
+    class CameraPageRenderer : PageRenderer
+    {
+        public override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+
+            // Disable swipe-to-go-back gesture
+            var navigationController = this.ViewController.NavigationController;
+            navigationController.InteractivePopGestureRecognizer.Enabled = false;
+        }
+    }
+}
